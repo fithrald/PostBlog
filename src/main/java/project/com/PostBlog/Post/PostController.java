e32dc6406797fb41ce849c9cdf57b3c230a7614f
Maksym Borshchev <eragon14000@gmail.com>
2024-01-19 12:40:57 +0200

    init commit

--- src/main/java/project/com/PostBlog/Post/PostController.java
+++ src/main/java/project/com/PostBlog/Post/PostController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
 //adss
     private final PostService postService;
-
+//asdds
     @Autowired
     public PostController(PostService postService) {
         this.postService = postService;
